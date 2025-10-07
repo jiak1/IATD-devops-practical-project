8611b9e5e355f13352f9788c4ad94a1df6444bd8
Jack Donaldson <jackdonaldson005@gmail.com>
2025-10-07 14:25:28 +1100

    Added a file to the repo

---
+++ lab1.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync'
+
+const myName = readlineSync.question('What is your name? ')
+
+console.log('Welcome to introduction to devops ' + myName)
+
+console.log('test')
